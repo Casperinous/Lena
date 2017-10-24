c314a35b80ca2a79a527043689f4f9059b64fae9
Casperinous <nikostotosis@gmail.com>
2017-10-25 01:07:40 +0300

    Changes: - Adding a variable in order to determine if a section has been modified. By checking the property,   we can select to either write the raw_data of the Androguard's objects based on our index or choose   to actually call the right write() function. - Determine the writesize of the objects. Items of specific sections have a constant size in the ge-   nerated Dex file.

--- section.py
+++ section.py
@@ -1,14 +1,16 @@
-
-
+from androguard.core.bytecodes.dvm import StringIdItem, TypeIdItem, ProtoIdItem, FieldIdItem, MethodIdItem, ClassDefItem
+from writer import dispatcher
 class Section():
 
-	def __init__(self,name,alignment,data,andro_object):
+	def __init__(self,name,alignment,data,andro_object, is_needed_in_header = False):
 		
 		self.__name = name
 		self.__alignment = alignment
 		#this is the raw data for sections which we dont change
 		if data:
 			self.__data = data
+		else 
+			self.__data = None
 		"""
 		Object could be a list of Androguard's class (list of proto_ids) 
 		or one class like the HeaderItem
@@ -20,6 +22,83 @@ class Section():
 		self.__write_size = 0
 		#Check if modified.
 		self.__is_modified = False
+		#Check if contributes to header's data
+		self.__is_needed_in_hd = is_needed_in_header
+		#Experimental Implementation of a callback function to be used when we are writing content in disk.
+		self.__callback = None
+
+
+	def _calculateWriteSize(self):
+
+		elem = None
+		if isinstance(self.__object, list):
+			elem = self.__object[0]
+		else
+			elem = self.__object
+
+		"""
+		According to source code we have that sections:
+		- header
+		- stringids
+		- typeids
+		- protoids
+		- fieldids
+		- methodids
+		- classdefs
+		are extending UniformItemSection which has the following
+		implementation to calculate WriteSize:
+		(It is a constant independed number instead on relying on
+		 the class value like StringDataItem)
+
+		public final int writeSize() {
+	        Collection<? extends Item> items = items();
+	        int sz = items.size();
+	        if (sz == 0) {
+	            return 0;
+	        }
+	        // Since each item has to be the same size, we can pick any.
+	        return sz * items.iterator().next().writeSize();
+    	}
+    	We can easily point out now that, in order to calculate the
+    	writesize of our section, we must first check what kind of
+    	class we are holding on and then, calculate the data size with
+    	the help of Androguard's functions.
+    	It is possible, due to being CPU-consuming, to perform some
+    	other calculations here too which are based on the kind of instance
+    	that self.__object is pointed to.
+    	"""
+
+    	#TODO - Combine in an OR clause the items that have the same writesize also use else if?
+		if isinstance(elem, StringIdItem)
+			#https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/StringIdItem.java#29
+			self.__write_size = 4
+
+		if isinstance(elem, TypeIdItem)
+			#https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/TypeIdItem.java#29
+			self.__write_size = 4
+
+		if isinstance(elem, ProtoIdItem)
+			#https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/ProtoIdItem.java#32
+			self.__write_size = 12
+
+		if isinstance(elem, FieldIdItem)
+			#https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/MemberIdItem.java#30
+			self.__write_size = 8
+
+		if isinstance(elem, MethodIdItem)
+			#https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/MemberIdItem.java#30
+			self.__write_size = 8
+
+		if isinstance(elem, ClassDefItem)
+			#https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/ClassDefItem.java#46
+			self.__write_size = 32
+
+		if isinstance(elem, StringDataItem):
+			#With a little luck, it might work ...
+			self.__callback = dispatcher['StringDataItem']
+
+
+
 
 
 	def setFileOff(self,file_off):
@@ -69,7 +148,7 @@ class Section():
 	def isModified():
 
 		return self.__is_modified
-		
+
 	def writeTo(self):
 		#To be implemented
 		pass
