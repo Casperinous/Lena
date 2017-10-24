918ca638f1cd3bdc9e85f1292babd2b7368fb5f0
Casperinous <nikostotosis@gmail.com>
2017-10-24 23:29:00 +0300

    Dummy test to see if an array is holding the reference after a function call. Javascript has messed me up.

--- test.py
+++ test.py
@@ -15,3 +15,23 @@ print '[*] Length of bytearray after extending it : {0}'.format(len(buff))
 
 #integer = struct.pack('<i', 'Lola')
 print ord('\0')
+
+
+class Test:
+
+	def __init__(self, arr):
+		self.arr = arr
+
+
+	def getArr(self):
+		return self.arr
+
+
+
+
+test = Test([0,1,2,3])
+
+n_arr = test.getArr()
+n_arr.append(4)
+
+print test.getArr()
