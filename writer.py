4c9948ffc5319a5d4dd45d58fd2491a3cdc33734
Casperinous <nikostotosis@gmail.com>
2017-10-25 01:15:59 +0300

    Trying to implement a dispatcher based on a Androguard's classes instance, basically in order to prevent checking again during the generation of the Dex file for the kind of class the section's __object is pointed to.

--- writer.py
+++ writer.py
@@ -1,6 +1,39 @@
 from utils import Data
 
 
+"""
+Experimental implementation of a dict with functions
+associated with the kind of class the __object prope-
+rty is pointed to in the Section class. It is kind 
+like reflection, but it is not.
+Any other suggestion would be considered a gift.
+"""
+
+def writeStringIdItem(self,writer,item):
+
+
+def writeStringDataItem(self, writer, item):
+	"""
+	ByteArray bytes = value.getBytes();
+	int utf16Size = value.getUtf16Size();
+	.....
+	out.writeUnsignedLeb128(utf16Size);
+	out.write(bytes);
+	out.writeByte(0);
+    """
+    writer.writeBytes(writeuleb128(item.get_utf16_size()))
+    writer.writeBytes(item.get_data())
+    writer.writeZeroes(1)
+
+
+dispatcher = {
+	'StringDataItem': writeStringDataItem
+}
+
+
+
+
+
 class Writer():
 
 
@@ -40,7 +73,7 @@ class Writer():
 
 		self._array.extend(data)
 
-	def writeZeroes(self, data, num):
+	def writeZeroes(self, num):
 		for i in range(0,num):
 			self._array.extend('\0')
 
