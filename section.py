0c5bc52a01984441e6efde84e01fc1aafc4541a6
Casperinous <nikostotosis@gmail.com>
2017-10-24 23:26:55 +0300

    High level implementation of a Section, as it is suggested by the original developers of the dexgen library. We, or for now i, tried to avoid defining multiple types of Sections and collected all the properties and functions i though are necessary.

--- section.py
+++ section.py
@@ -2,17 +2,27 @@
 
 class Section():
 
-	def __init__(self,name,alignment,data):
+	def __init__(self,name,alignment,data,andro_object):
+		
 		self.__name = name
 		self.__alignment = alignment
-		self.__data = data
+		#this is the raw data for sections which we dont change
+		if data:
+			self.__data = data
+		"""
+		Object could be a list of Androguard's class (list of proto_ids) 
+		or one class like the HeaderItem
+		"""
+		self.__object = andro_object
 		#should be set later on during the write process
 		self.__file_off = 0
 		#Should be computed based on the available data
 		self.__write_size = 0
+		#Check if modified.
+		self.__is_modified = False
 
 
-	def set_file_off(self,file_off):
+	def setFileOff(self,file_off):
 
 		'''
 		Straight from the Android's source code, mapping one to one
@@ -35,11 +45,32 @@ class Section():
 		file_off = ( file_off + mask ) & ~mask
 		self.__file_off = file_off
 
-	def get_file_off(self):
+	def getFileOff(self):
 
 		return self.__file_off
 
-	def get_abs_file_off(relative):
+	def getAbsFileOff(relative):
 
 		return self.__file_off + relative
+
+	def addItem(self,obj):
+		#We assume that, we have a list of same objects.
+		if isinstance(self.__object, list):
+			self.__object.append(obj)
+
+	def getRawData(self):
+		
+		return self.data
+
+	def getAndroguardObj(self):
+
+		return self.__object
+
+	def isModified():
+
+		return self.__is_modified
+		
+	def writeTo(self):
+		#To be implemented
+		pass
 
