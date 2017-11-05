523c6233406c239b2acd3c4c39b0a06846a7f74c
Casperinous <nikostotosis@gmail.com>
2017-11-05 13:25:11 +0200

    General tests

---
+++ test/test.py
@@ -0,0 +1,20 @@
+import sys
+from androguard.core.bytecodes import dvm
+from androguard.core.bytecodes import apk
+from androguard.core.bytecodes.dvm import TYPE_MAP_ITEM
+
+sys.path.append('../')
+from dex import Dex
+
+FILENAME_INPUT = "../app-debug.apk"
+
+
+a = apk.APK(FILENAME_INPUT)
+vm = dvm.DalvikVMFormat(a.get_dex())
+
+
+dex = Dex(vm.map_list)
+
+if dex:
+	dex.initSections()
+	dex.prepareSections()
