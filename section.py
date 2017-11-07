f4b01ee3009c608f2056da50802ddbdd3efe6201
Casperinous <nikostotosis@gmail.com>
2017-11-07 23:50:24 +0200

    Really, where to begin here. After 2 days, i learned with the hard way, about name mangling. My super() constructor was actually behaved as expected. The problem was that, properties defined with underscore were protected by compiler, thus removing access to them from the superclass. A really amateur mistake, proving how dump my python code was. In other news fixes include: - Defining all the properties without undercore. - Realizing that, most of the objects passed to Section/MixSection constructors were not a list of *Items, but rather an object holding a list of *Items. Many changes were performed to self.data = ? initialization in order to correctly assigned a list object. - Finally, implementation of the callback function, used in the writeTo method. It seems to perform better than expected. For now at least. - Changes in import types + some prints for debugging purposes

--- section.py
+++ section.py
@@ -1,4 +1,4 @@
-from androguard.core.bytecodes.dvm import StringIdItem, TypeIdItem, ProtoIdItem, FieldIdItem, MethodIdItem, ClassDefItem
+from androguard.core.bytecodes.dvm import StringIdItem, TypeHIdItem, ProtoHIdItem, FieldHIdItem, MethodHIdItem, ClassHDefItem, StringDataItem, ClassDataItem, TypeList, MapList
 #from utils import SectionWriter
 from items import OffsettedItem
 from utils import Data
@@ -9,35 +9,33 @@ class Section(OffsettedItem):
 
     def __init__(self, typeid, name, alignment, data, andro_object, is_needed_in_header=False):
 
-        super(Section, self).__init__(alignment)
-        print self.__alignment
-        self.__name = name
+        OffsettedItem.__init__(self, alignment)
+        self.name = name
+        print 'Section name :> [{0}]'.format(self.name)
         # this is the raw data for sections which we dont change
         if data:
-            self.__data = data
+            self.data = data
         else:
-            self.__data = None
+            self.data = None
         """
         Object could be a list of Androguard's class (list of proto_ids) 
         or one class like the HeaderItem
         """
-        self.__object = andro_object
+        self.object = andro_object
         # Check if modified.
-        self.__is_modified = False
+        self.is_modified = False
         # Check if contributes to header's data
-        self.__is_needed_in_hd = is_needed_in_header
+        self.is_needed_in_hd = is_needed_in_header
         # Experimental Implementation of a callback function to be used when we
         # are writing content in disk.
-        self.__callback = None
+        self.callback = None
         # Expected a short number here.
-        self.__typeid = typeid
-
+        self.typeid = typeid
 
 
     def _placeItems(self):
 
-        elem = Data.getInstance(self.__object)
-
+        elem = Data.getInstance(self.object)
         """
         According to source code we have that sections:
         - header
@@ -67,92 +65,94 @@ class Section(OffsettedItem):
         the help of Androguard's functions.
         It is possible, due to being CPU-consuming, to perform some
         other calculations here too which are based on the kind of instance
-        that self.__object is pointed to.
+        that self.object is pointed to.
         """
 
         # TODO - Combine in an OR clause the items that have the same writesize
         # also use else if?
         if isinstance(elem, StringIdItem):
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/StringIdItem.java#29
-            self.__instance_write_size = 4
-            # Assign array of Items to .__data property
-            self.__data = self.__object
+            self.instance_write_size = 4
+            # Assign array of Items to .data property
+            self.data = self.object
             # Set write size
-            self.__write_size = self.__instance_write_size * len(self.__data)
+            self.write_size = self.instance_write_size * len(self.data)
             # set write callback
-            self.__callback = SectionWriter.writeStringIdSection
+            self.callback = SectionWriter.writeStringIdSection
 
-        if isinstance(elem, TypeIdItem):
+        if isinstance(elem, TypeHIdItem):
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/TypeIdItem.java#29
-            self.__instance_write_size = 4
-            # Assign array of Items to .__data property
-            self.__data = self.__object
-            self.__write_size = self.__instance_write_size * len(self.__data)
-            self.__callback = SectionWriter.writeTypeIdSection
+            self.instance_write_size = 4
+            # Assign array of Items to .data property
+            self.data = self.object.get_type()
+            self.write_size = self.instance_write_size * len(self.data)
+            self.callback = SectionWriter.writeTypeIdSection
 
-        if isinstance(elem, ProtoIdItem):
+        if isinstance(elem, ProtoHIdItem):
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/ProtoIdItem.java#32
-            self.__instance_write_size = 12
-            # Assign array of Items to .__data property
-            self.__data = self.__object
-            self.__write_size = self.__instance_write_size * len(self.__data)
-            self.__callback = SectionWriter.writeProtoIdSection
+            self.instance_write_size = 12
+            # Assign array of Items to .data property
+            self.data = self.object.get_obj()
+            self.write_size = self.instance_write_size * len(self.data)
+            self.callback = SectionWriter.writeProtoIdSection
 
-        if isinstance(elem, FieldIdItem):
+        if isinstance(elem, FieldHIdItem):
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/MemberIdItem.java#30
-            self.__instance_write_size = 8
-            # Assign array of Items to .__data property
-            self.__data = self.__object
-            self.__write_size = self.__instance_write_size * len(self.__data)
-            self.__callback = SectionWriter.writeFieldIdSection
+            self.instance_write_size = 8
+            # Assign array of Items to .data property
+            self.data = self.object.get_obj()
+            self.write_size = self.instance_write_size * len(self.data)
+            self.callback = SectionWriter.writeFieldIdSection
 
-        if isinstance(elem, MethodIdItem):
+        if isinstance(elem, MethodHIdItem):
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/MemberIdItem.java#30
-            self.__instance_write_size = 8
-            # Assign array of Items to .__data property
-            self.__data = self.__object
-            self.__write_size = self.__instance_write_size * len(self.__data)
-            self.__callback = SectionWriter.writeMethodIdSection
+            self.instance_write_size = 8
+            # Assign array of Items to .data property
+            self.data = self.object.get_obj()
+            self.write_size = self.instance_write_size * len(self.data)
+            self.callback = SectionWriter.writeMethodIdSection
 
-        if isinstance(elem, ClassDefItem):
+        if isinstance(elem, ClassHDefItem):
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/ClassDefItem.java#46
-            self.__instance_write_size = 32
-            # Assign array of Items to .__data property
-            self.__data = self.__object
-            self.__write_size = self.__instance_write_size * len(self.__data)
-            self.__callback = SectionWriter.writeClassDefSection
+            self.instance_write_size = 32
+            # Assign array of Items to .data property
+            self.data = self.object.get_obj()
+            self.write_size = self.instance_write_size * len(self.data)
+            self.callback = SectionWriter.writeClassDefSection
+
 
     def addItem(self, obj):
         # We assume that, we have a list of same objects.
-        if isinstance(self.__data, list):
-            self.__data.append(obj)
+        if isinstance(self.data, list):
+            self.data.append(obj)
 
     def getRawData(self):
 
-        return self.__data
+        return self.data
 
     def getAndroguardObj(self):
 
-        return self.__object
+        return self.callback
 
     def isModified(self):
 
-        return self.__is_modified
+        return self.is_modified
 
     def setModified(value):
 
-        self.__is_modified = value
+        self.is_modified = value
 
     def getTypeId(self):
 
-        return self.__typeid
+        return self.typeid
 
     def placeItems():
         pass
 
-    def writeTo(self):
-        # To be implemented
-        pass
+    def writeTo(self, dex):
+        if self.callback:
+            self.callback(self.data, self, dex)
+        
 
     def prepareSection(self):
         self._placeItems()
@@ -160,26 +160,29 @@ class Section(OffsettedItem):
     def writeHeaderPart(self, writer):
         pass
 
+    def getName(self):
+
+        return self.name
 
 class MixedSection(Section):
 
-    """
+    
     def __init__(self, typeid, name, alignment, data, andro_object, is_needed_in_header=False):
 
         super(MixedSection, self).__init__(typeid, name, alignment, data, andro_object, is_needed_in_header)
-    """
+    
 
     def _placeItems(self):
 
         
-        elem = Data.getInstance(self.__object)
+        elem = Data.getInstance(self.getAndroguardObj())
         writer_offset = 0
 
         if isinstance(elem, StringDataItem):
-            self.__data = self.__object
-            self.__callback = SectionWriter.writeStringDataSection
+            self.data = self.object
+            self.callback = SectionWriter.writeStringDataSection
             # How convinient to have access to such great methods :)
-            for item in self.__object:
+            for item in self.object:
                 # https://github.com/androguard/androguard/blob/v2.0/androguard/core/bytecodes/dvm.py#L1775
                 off = Data.toAligned(self.__aligment, writer_offset)
                 item.set_off(off)
@@ -188,12 +191,12 @@ class MixedSection(Section):
                     (writeuleb128(item.get_utf16_size) + len(item.get_data()) + 1)
 
             # With a little luck, it might work ...
-            self.__callback = SectionWriter.writeStringDataSection
+            self.callback = SectionWriter.writeStringDataSection
 
         elif isinstance(elem, ClassDataItem):
-            self.__data = self.__object
-            self.__callback = SectionWriter.writeClassDataSection
-            for item in self.__object:
+            self.data = self.object
+            self.callback = SectionWriter.writeClassDataSection
+            for item in self.object:
                 off = Data.toAligned(self.__aligment, writer_offset)
                 item.set_off(off)
                 # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/MixedItemSection.java#319
@@ -201,46 +204,46 @@ class MixedSection(Section):
                 writer_offset = off + item.get_length()
 
         elif isinstance(elem, TypeList):
-            self.__data = self.__object.get_list()
-            self.__callback = SectionWriter.writeTypeListSection
+            self.data = self.object.get_list()
+            self.callback = SectionWriter.writeTypeListSection
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/TypeListItem.java#48
-            self.__instance_write_size = 2
-            self.__write_size = len(
-                self.__data * self.__instance_write_size) + 4
+            self.instance_write_size = 2
+            self.write_size = len(
+                self.data * self.instance_write_size) + 4
             '''
             Androguard does not have at v2 functions or properties related
             to offsets.
             So, this :
-            for item in self.__object.get_list():
+            for item in self.object.get_list():
                 off = Data.toAligned(writer_offset)
                 item.off = off
-                writer_offset = off + self.__instance_write_size
+                writer_offset = off + self.instance_write_size
             Is not necessary for now.
             '''
 
         elif isinstance(elem, MapList):
-            self.__data = self.__object.map_item
+            self.data = self.object.map_item
             # https://android.googlesource.com/platform/dalvik/+/master/dexgen/src/com/android/dexgen/dex/file/MapItem.java#32
-            self.__instance_write_size = 3 * 4
-            self.__callback = Section.writeMapItemSection
-            self.__write_size = len(
-                self.__object.get_obj()) * self.__instance_write_size
+            self.instance_write_size = 3 * 4
+            self.callback = Section.writeMapItemSection
+            self.write_size = len(
+                self.object.get_obj()) * self.instance_write_size
             # Abuse of property - wish there was a function instead for optical
             # pleasure :/
-            for item in self.__data:
+            for item in self.data:
                 off = Data.toAligned(self.__aligment, writer_offset)
                 # Abuse of property again :/ :/
                 item.off = off
-                writer_offset = off + self.__instance_write_size
+                writer_offset = off + self.instance_write_size
 
         # Set  write size
-        if self.__write_size == 0:
-            self.__write_size = writer_offset
+        if self.write_size == 0:
+            self.write_size = writer_offset
 
     def prepareSection(self):
         self._placeItems()
 
     def writeHeaderPart(self, writer):
 
-        writer.writeSignedInt(len(self.__data))
+        writer.writeSignedInt(len(self.data))
         writer.writeSignedInt(self.getFileOff())
